36cb4d7aed47b96c01e28724a5079c3cbfa31fe9
Josh Croff <joshua.croff@gmail.com>
2015-05-24 16:10:20 -0700

    Fix Bcrypt, add Bcrypt validation, fix login

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,22 +17,23 @@ post '/signup' do
                   first_name: params[:first_name],
                   last_name: params[:last_name],
                   username: params[:username],
-                  password_hash: BCrypt::Password.create(params[:password]),
                   email: params[:email],
                   charity_id: params[:charity_id]
                   )
+  user.password = params[:password]
+
   if user.save
     session[:user_id] = user.id
     redirect '/home/dashboard'
   else
-    # redirect '/signup'
+    redirect '/home/signup'
   end
 end
 
 post '/login' do
-  password_hash = BCrypt::Password.create(params[:password])
+  password_hash = params[:password]
   user = User.find_by(username: params[:username])
-  if user && user.password_hash == password_hash
+  if user && user.password == password_hash
     session[:user_id] = user.id
     redirect '/home/dashboard'
   else
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,28 +2,33 @@ require 'faker'
 
 20.times do
   User.create(
-    first_name: Faker::Name.first_name
-    last_name: Faker::Name.last_name
-    username: Faker::Internet.user_name
-    password_hash: Faker::Internet.password
-    email: Faker::Internet.email
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Internet.user_name,
+    password_hash: Faker::Internet.password,
+    email: Faker::Internet.email,
     charity_id: 1
     )
 end
 
 Charity.create(
-  name: "Community Housing Partnership"
-  street_address: "20 Jones Street"
-  secondary_address: "Suite 200"
-  city: "San Francisco"
-  state: "California"
-  zip: "94102"
-  phone: "4158525300"
+  name: "Community Housing Partnership",
+  street_address: "20 Jones Street",
+  secondary_address: "Suite 200",
+  city: "San Francisco",
+  state: "California",
+  zip: "94102",
+  phone: "4158525300",
   payment_method: "Website"
   )
 
-Transaction.create()
+1000.times do
+  Transaction.create(
+    amount: Faker::Commerce.price,
+    user_id: rand(19) + 1
+    )
+end
 
 
 
-
+#rand(1..9)
